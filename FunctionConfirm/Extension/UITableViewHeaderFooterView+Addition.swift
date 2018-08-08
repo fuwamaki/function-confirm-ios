6ea50ab48d73ea28121a6621638c8182e06fadb5
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 11:14:38 +0900

    UITableViewCellとUITableViewHeaderFooterViewのextensionファイルを作成

---
+++ FunctionConfirm/Extension/UITableViewHeaderFooterView+Addition.swift
@@ -0,0 +1,20 @@
+//
+//  UITableViewHeaderFooterView+Addition.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewHeaderFooterView {
+    
+    class func defaultHeight(_ tableView: UITableView) -> CGFloat {
+        return 32.0
+    }
+    
+    class var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
