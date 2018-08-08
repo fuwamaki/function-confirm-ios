64e661fa13b33a87063ea2aba629a8f3bb0f2090
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 23:18:34 +0900

    init使わずにlabelを定義

--- FunctionConfirm/MVP/Regist/MVPRegistViewController.swift
+++ FunctionConfirm/MVP/Regist/MVPRegistViewController.swift
@@ -11,22 +11,20 @@ import Cartography
 
 class MVPRegistViewController: UIViewController {
 
-    private var itemNameLabel: UILabel
+    private let itemNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.red
+        label.text = "てすと"
+        return label
+    }()
 
     private struct Text {
         static let title = "商品登録"
         static let close = "戻る"
     }
 
-    init() {
-        itemNameLabel = UILabel()
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = Text.title
@@ -40,10 +38,6 @@ class MVPRegistViewController: UIViewController {
     }
 
     private func setLabels() {
-        itemNameLabel.font = UIFont.systemFont(ofSize: 14.0)
-        itemNameLabel.textColor = UIColor.black
-        itemNameLabel.backgroundColor = UIColor.red
-        itemNameLabel.text = "てすと"
         view.addSubview(itemNameLabel)
         constrain(itemNameLabel) { label in
             label.width == 100
