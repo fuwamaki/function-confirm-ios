8aaf121e46c8b8ab46e1a5a00f956b9851b21b6f
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 10:40:04 +0900

    Presenterファイル作成

---
+++ FunctionConfirm/MVP/MVPPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  MVPPresenter.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import Foundation
+
+protocol MVPPresentable: class {
+    
+}
+
+class MVPPresenter {
+
+}
+
+extension MVPPresenter: MVPPresentable {
+    
+}
