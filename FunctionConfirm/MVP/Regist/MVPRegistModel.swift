c1f42bc261a28f6a574f7e64026087c13a1d5093
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 11:51:23 +0900

    商品登録画面用のファイルを作成

---
+++ FunctionConfirm/MVP/Regist/MVPRegistModel.swift
@@ -0,0 +1,9 @@
+//
+//  MVPRegistModel.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import Foundation
---
+++ FunctionConfirm/MVP/Regist/MVPRegistViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MVPRegistViewController.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import Foundation
