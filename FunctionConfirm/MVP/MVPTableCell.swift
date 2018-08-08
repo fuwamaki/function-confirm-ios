071a0eab3e3ed9931b4ba671f828733107fc3e6a
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 11:11:27 +0900

    TableCellファイルを作成

---
+++ FunctionConfirm/MVP/MVPTableCell.swift
@@ -0,0 +1,28 @@
+//
+//  MVPTableCell.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import UIKit
+
+final class MVPTableCell: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    private func setupView() {
+    }
+
+    func setItem() {
+    }
+}
