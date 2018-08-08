4efc7027935d6d79f3d0f8391f089a5bcf167324
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-08 10:46:03 +0900

    Modelファイル作成

---
+++ FunctionConfirm/MVP/MVPModel.swift
@@ -0,0 +1,21 @@
+//
+//  MVPModel.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 8/8/18.
+//  Copyright © 2018 牧宥作. All rights reserved.
+//
+
+import Foundation
+
+protocol MVPModelInterface: class {
+    
+}
+
+class MVPModel {
+    
+}
+
+extension MVPModel: MVPModelInterface {
+
+}
