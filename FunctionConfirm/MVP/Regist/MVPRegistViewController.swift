a498901700dc845e9c9bc18a9b5d418a1554e787
fuwamaki <variety.fuwa.3@gmail.com>
2018-08-09 12:05:03 +0900

    登録画面のレイアウト要素配置完了

--- FunctionConfirm/MVP/Regist/MVPRegistViewController.swift
+++ FunctionConfirm/MVP/Regist/MVPRegistViewController.swift
@@ -9,20 +9,75 @@
 import UIKit
 import Cartography
 
+private struct Text {
+    static let title = "商品登録"
+    static let close = "戻る"
+    static let name = "商品名"
+    static let namePlaceholder = "商品名を入力"
+    static let category = "カテゴリ"
+    static let categoryPlaceholder = "カテゴリを選択"
+    static let price = "価格"
+    static let pricePlaceholder = "価格を入力"
+    static let regist = "登録"
+}
+
 class MVPRegistViewController: UIViewController {
 
-    private struct Text {
-        static let title = "商品登録"
-        static let close = "戻る"
-    }
-
-    private let itemNameLabel: UILabel = {
+    private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14.0)
         label.textColor = UIColor.black
-        label.backgroundColor = UIColor.red
-        label.text = "てすと"
+        label.text = Text.name
         return label
+    }()
+
+    private lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.textColor = UIColor.black
+        label.text = Text.category
+        return label
+    }()
+
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.textColor = UIColor.black
+        label.text = Text.price
+        return label
+    }()
+
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 14.0)
+        textField.textColor = UIColor.black
+        textField.placeholder = Text.namePlaceholder
+        return textField
+    }()
+
+    private lazy var categoryTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 14.0)
+        textField.textColor = UIColor.black
+        textField.placeholder = Text.categoryPlaceholder
+        return textField
+    }()
+
+    private lazy var priceTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 14.0)
+        textField.textColor = UIColor.black
+        textField.placeholder = Text.pricePlaceholder
+        return textField
+    }()
+
+    private lazy var registButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Text.regist, for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14.0)
+        button.backgroundColor = UIColor.red
+        return button
     }()
 
     override func viewDidLoad() {
@@ -34,20 +89,54 @@ class MVPRegistViewController: UIViewController {
 
     override func viewDidLayoutSubviews() {
         // MEMO: viewDidLayoutSubviews内じゃないとview.safeAreaInsets.topの値を取得できない
-        setLabels(view.safeAreaInsets.top)
+        setViews(view.safeAreaInsets.top)
     }
 
     @objc func close(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
 
-    private func setLabels(_ topMargin: CGFloat) {
-        view.addSubview(itemNameLabel)
-        constrain(itemNameLabel) { label in
-            label.width == 100
-            label.height == 50
-            label.top == label.superview!.top + topMargin
-            label.left == label.superview!.left
+    private func setViews(_ topMargin: CGFloat) {
+        view.addSubview(nameLabel)
+        view.addSubview(categoryLabel)
+        view.addSubview(priceLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(categoryTextField)
+        view.addSubview(priceTextField)
+        view.addSubview(registButton)
+        constrain(nameLabel, categoryLabel, priceLabel) { nameLabel, categoryLabel, priceLabel in
+            nameLabel.width == 64
+            nameLabel.height == 32
+            nameLabel.top == nameLabel.superview!.top + topMargin + 120
+            nameLabel.left == nameLabel.superview!.left + 48
+            categoryLabel.width == 64
+            categoryLabel.height == 32
+            categoryLabel.top == nameLabel.bottom + 48
+            categoryLabel.left == categoryLabel.superview!.left + 48
+            priceLabel.width == 64
+            priceLabel.height == 32
+            priceLabel.top == categoryLabel.bottom + 48
+            priceLabel.left == priceLabel.superview!.left + 48
+        }
+        constrain(nameTextField, categoryTextField, priceTextField) { nameTextField, categoryTextField, priceTextField in
+            nameTextField.width == 120
+            nameTextField.height == 32
+            nameTextField.top == nameTextField.superview!.top + topMargin + 120
+            nameTextField.right == nameTextField.superview!.right - 48
+            categoryTextField.width == 120
+            categoryTextField.height == 32
+            categoryTextField.top == nameTextField.bottom + 48
+            categoryTextField.right == nameTextField.superview!.right - 48
+            priceTextField.width == 120
+            priceTextField.height == 32
+            priceTextField.top == categoryTextField.bottom + 48
+            priceTextField.right == nameTextField.superview!.right - 48
+        }
+        constrain(registButton) { button in
+            button.height == 48
+            button.left == button.superview!.left + 48
+            button.right == button.superview!.right - 48
+            button.bottom == button.superview!.bottom - 120
         }
     }
 }
