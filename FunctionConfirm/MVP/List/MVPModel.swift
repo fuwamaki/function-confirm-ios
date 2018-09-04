e6610de4cd0aac77448c406f2abebe8cb42324a0
fuwamaki <variety.fuwa.3@gmail.com>
2018-09-04 23:57:49 +0900

    fix item-server URL

--- FunctionConfirm/MVP/List/MVPModel.swift
+++ FunctionConfirm/MVP/List/MVPModel.swift
@@ -8,14 +8,55 @@
 
 import Foundation
 
-protocol MVPModelInterface: class {
-    
+protocol MVPNotify: class {
+    func addObserver(_ observer: Any, selector: Selector)
+    func removeObserver(_ observer: Any)
+}
+
+protocol MVPModelInterface: MVPNotify {
+    var items: [Item] { get }
+    func getItems()
 }
 
 class MVPModel {
-    
+
+    private(set) var items: [Item] = []
+    private let api: RESTfulApiRequest
+
+    required init(api: RESTfulApiRequest) {
+        self.api = api
+    }
 }
 
 extension MVPModel: MVPModelInterface {
 
+    func getItems() {
+        api.getAPI { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.items = response.data
+                self?.notify()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension MVPModel: MVPNotify {
+    var notificationName: Notification.Name {
+        return Notification.Name(rawValue: "mvpNotify")
+    }
+
+    func addObserver(_ observer: Any, selector: Selector) {
+        NotificationCenter.default.addObserver(observer, selector: selector, name: notificationName, object: nil)
+    }
+
+    func removeObserver(_ observer: Any) {
+        NotificationCenter.default.removeObserver(observer)
+    }
+
+    func notify() {
+        NotificationCenter.default.post(name: notificationName, object: nil)
+    }
 }
