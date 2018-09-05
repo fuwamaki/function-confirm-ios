0d898f0afc4ecd218fdc270ceb4533eef2eb56fb
fuwamaki <variety.fuwa.3@gmail.com>
2018-09-05 12:38:52 +0900

    add ItemRequest

---
+++ FunctionConfirm/MVP/ItemRequest.swift
@@ -0,0 +1,17 @@
+//
+//  ItemRequest.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2018/09/05.
+//  Copyright © 2018年 牧宥作. All rights reserved.
+//
+
+import Foundation
+
+struct ItemRequest: Codable {
+    let item: Item
+    
+    private enum CodingKeys: String, CodingKey {
+        case item = "item"
+    }
+}
