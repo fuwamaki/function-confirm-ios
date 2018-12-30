e5621d3ed3cc3355c9ac37ca430ba1021760d158
fuwamaki <variety.fuwa.3@gmail.com>
2018-12-30 16:02:43 +0900

    add necessary files for MVVM

---
+++ FunctionConfirm/MVVM/MVVMListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MVVMListViewModel.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2018/12/30.
+//  Copyright © 2018年 牧宥作. All rights reserved.
+//
+
+import Foundation
