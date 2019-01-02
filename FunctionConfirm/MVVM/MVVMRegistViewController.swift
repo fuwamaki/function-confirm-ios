4eb4f70994e146d1aaa631153cc241197759e10d
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-03 03:37:43 +0900

    add default setting on MVVMRegist

--- FunctionConfirm/MVVM/MVVMRegistViewController.swift
+++ FunctionConfirm/MVVM/MVVMRegistViewController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2018年 牧宥作. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class MVVMRegistViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+
+    private lazy var viewModel: MVVMRegistViewModel = {
+        return MVVMRegistViewModel(viewController: self)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- FunctionConfirm/MVVM/MVVMRegistViewModel.swift
+++ FunctionConfirm/MVVM/MVVMRegistViewModel.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2018年 牧宥作. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class MVVMRegistViewModel {
+
+    private let viewController: UIViewController
+    private let apiRequest: ItemAPIRequestRxProtocol
+    private let disponseBag = DisposeBag()
+
+    // for viewController
+    convenience init(viewController: UIViewController) {
+        self.init(viewController: viewController, request: ItemAPIRequestRx())
+    }
+
+    // for test
+    init(viewController: UIViewController, request: ItemAPIRequestRxProtocol) {
+        self.viewController = viewController
+        self.apiRequest = request
+    }
+}
