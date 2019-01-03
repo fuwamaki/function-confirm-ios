a4f576fa1ace107c342308248b064a7b13a2392b
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-04 02:39:11 +0900

    set mvvm list view controller like sample system

--- FunctionConfirm/MVVM/MVVMListViewController.swift
+++ FunctionConfirm/MVVM/MVVMListViewController.swift
@@ -7,41 +7,89 @@
 //
 
 import UIKit
+import KRProgressHUD
 import RxSwift
+import RxCocoa
 
 class MVVMListViewController: UIViewController {
 
-    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.registerForCell(MVVMListTableViewCell.self)
+        }
+    }
+    @IBOutlet private weak var registNavigationBarButtonItem: UIBarButtonItem!
+    
+    private let disposeBag = DisposeBag()
+    var navigationBarTitle = BehaviorRelay(value: "MVVM List")
 
     private lazy var viewModel: MVVMListViewModel = {
         return MVVMListViewModel(viewController: self)
     }()
 
     override func viewDidLoad() {
-        setupTableView()
+        super.viewDidLoad()
+        bind()
+        viewModel.fetchItems().subscribe().disposed(by: disposeBag)
     }
 
-    private func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.backgroundColor = UIColor.baseGray
-    }
-}
-
-extension MVVMListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    private func openMVVMRegistViewController() {
+        let storyBoard = UIStoryboard(name: "MVVMRegist", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "MVVMRegistViewController")
+        let navigationController = UINavigationController(rootViewController: vc)
+        present(navigationController, animated: true, completion: nil)
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MVPTableCell", for: indexPath)
-        return cell
+    private func showErrorAlert(message: String) {
+        let alert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
-}
 
-extension MVVMListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    func bind() {
+        navigationBarTitle
+            .bind(to: navigationItem.rx.title)
+            .disposed(by: disposeBag)
+
+        registNavigationBarButtonItem.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.openMVVMRegistViewController()
+            })
+            .disposed(by: disposeBag)
+
+        tableView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                self?.openMVVMRegistViewController()
+                self?.tableView.deselectRow(at: indexPath, animated: false)
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.state
+            .drive(onNext: { state in
+                switch state {
+                case .idle:
+                    break
+                case .itemsFetching:
+                    KRProgressHUD.show()
+                case .itemsFetchCompleted:
+                    KRProgressHUD.dismiss()
+                case .errorOccurred(let error):
+                    KRProgressHUD.dismiss()
+                    self.showErrorAlert(message: error.message)
+                }
+            })
+            .disposed(by: disposeBag)
+
+        // TODO: tableviewどうにかする
+        viewModel.items
+            .drive(tableView.rx.items) { tableView, _/*index*/, element in
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: MVVMListTableViewCell.defaultReuseIdentifier) as? MVVMListTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.render(item: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
     }
 }
 
--- FunctionConfirm/MVVM/MVVMListViewModel.swift
+++ FunctionConfirm/MVVM/MVVMListViewModel.swift
@@ -73,24 +73,23 @@ class MVVMListViewModel {
         self.apiRequest = request
     }
 
-
     func fetchItems() -> Completable {
         stateSubject.accept(.itemsFetching)
         return apiRequest.getItems()
-        .do(
-            onSuccess: { [weak self] response in
-                self?.itemsSubject.accept([])
-                self?.itemsSubject.accept(response.data)
-                self?.stateSubject.accept(.itemsFetchCompleted)
-                self?.stateSubject.accept(.idle)
-            },
-            onError: { [weak self] error in
-                print("Error: \(error)")
-                self?.stateSubject.accept(.errorOccurred(.responseFailure))
-                self?.stateSubject.accept(.idle)
-            }
-        )
+            .do(
+                onSuccess: { [weak self] response in
+                    self?.itemsSubject.accept([])
+                    self?.itemsSubject.accept(response.data)
+                    self?.stateSubject.accept(.itemsFetchCompleted)
+                    self?.stateSubject.accept(.idle)
+                },
+                onError: { [weak self] error in
+                    print("Error: \(error)")
+                    self?.stateSubject.accept(.errorOccurred(.responseFailure))
+                    self?.stateSubject.accept(.idle)
+                }
+            )
             .map { _ in } // Single<Void>に変換
-        .asCompletable() // Completableに変換
+            .asCompletable() // Completableに変換
     }
 }
