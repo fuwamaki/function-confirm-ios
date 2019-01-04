de65626ac15b58687850d12e18ddca2097bf51e5
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-04 18:22:54 +0900

    create MVVMRegist main function

--- FunctionConfirm/MVVM/MVVMRegistViewController.swift
+++ FunctionConfirm/MVVM/MVVMRegistViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxSwift
+import KRProgressHUD
 
 class MVVMRegistViewController: UIViewController {
 
@@ -32,13 +33,121 @@ class MVVMRegistViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+        bindNavigationBar()
+        bindDismiss()
+        bindSubmitItem()
+        bindTextFields()
+        bindButton()
+        bindState()
     }
 
-    func bind() {
+    private func showErrorAlert(message: String) {
+        let alert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    private func bindDismiss() {
+        viewModel.dismissSubject
+            .subscribe(onNext: { [weak self] isDismiss in
+                if isDismiss {
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            })
+        .disposed(by: disposeBag)
+    }
+
+    private func bindNavigationBar() {
+        viewModel.navigationBarTitle
+            .bind(to: navigationItem.rx.title)
+            .disposed(by: disposeBag)
+
         closeNavigationBarButtonItem.rx.tap
             .subscribe(onNext: { [weak self] in
                 self?.dismiss(animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func bindSubmitItem() {
+        viewModel.submitItem
+            .flatMap { $0.flatMap{ Observable.just($0) } ?? Observable.empty() }
+            .subscribe(onNext: { [weak self] item in
+                self?.viewModel.itemId.accept(item.id)
+                self?.viewModel.nameText.accept(item.name)
+                self?.viewModel.categoryText.accept(item.category)
+                self?.viewModel.priceText.accept(String(item.price))
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.submitItem
+            .filter { $0 == nil }
+            .subscribe(onNext: { [weak self] _ in
+                self?.viewModel.nameText.accept(nil)
+                self?.viewModel.categoryText.accept(nil)
+                self?.viewModel.priceText.accept(nil)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func bindTextFields() {
+        viewModel.nameText
+            .asDriver(onErrorJustReturn: "")
+            .drive(nameTextField.rx.text)
+            .disposed(by: disposeBag)
+
+        nameTextField.rx.text.orEmpty
+            .bind(to: viewModel.nameText)
+            .disposed(by: disposeBag)
+
+        viewModel.categoryText
+            .asDriver(onErrorJustReturn: "")
+            .drive(categoryTextField.rx.text)
+            .disposed(by: disposeBag)
+
+        categoryTextField.rx.text.orEmpty
+            .bind(to: viewModel.categoryText)
+            .disposed(by: disposeBag)
+
+        viewModel.priceText
+            .asDriver(onErrorJustReturn: "")
+            .drive(priceTextField.rx.text)
+            .disposed(by: disposeBag)
+
+        priceTextField.rx.text.orEmpty
+            .bind(to: viewModel.priceText)
+            .disposed(by: disposeBag)
+    }
+
+    private func bindButton() {
+        viewModel.allFieldsValid
+            .bind(to: submitButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        submitButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                if let weakSelf = self {
+                    weakSelf.viewModel.postItem().subscribe().disposed(by: weakSelf.disposeBag)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func bindState() {
+        viewModel.state
+            .drive(onNext: { [weak self] state in
+                switch state {
+                case .idle:
+                    break
+                case .itemSubmitting:
+                    KRProgressHUD.show()
+                case .itemSubmitCompleted:
+                    KRProgressHUD.dismiss()
+                    self?.submitCompletedSubject.onNext(())
+                case .errorOccurred(let error):
+                    KRProgressHUD.dismiss()
+                    self?.showErrorAlert(message: error.message)
+                }
             })
             .disposed(by: disposeBag)
     }
--- FunctionConfirm/MVVM/MVVMRegistViewModel.swift
+++ FunctionConfirm/MVVM/MVVMRegistViewModel.swift
@@ -8,12 +8,58 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
+// TODO: RegistじゃなくてSubmitにする
 class MVVMRegistViewModel {
 
+    enum SubmitMode {
+        case new
+        case update
+    }
+
+    // viewController消す
     private let viewController: UIViewController
     private let apiRequest: ItemAPIRequestRxProtocol
     private let disponseBag = DisposeBag()
+
+    var navigationBarTitle = BehaviorRelay(value: "Regist")
+    var itemId = BehaviorRelay<Int?>(value: nil)
+    var nameText = BehaviorRelay<String?>(value: nil)
+    var categoryText = BehaviorRelay<String?>(value: nil)
+    var priceText = BehaviorRelay<String?>(value: nil)
+    var submitMode = BehaviorRelay<SubmitMode>(value: .new)
+    var submitItem = BehaviorRelay<ItemRx?>(value: nil)
+
+    lazy var nameValid: Observable<Bool> = {
+        return nameText
+            .map { ($0 ?? "").count > 0 }
+            .share(replay: 1)
+    }()
+
+    lazy var categoryValid: Observable<Bool> = {
+        return categoryText
+            .map{ ($0 ?? "").count > 0}
+            .share(replay: 1)
+    }()
+
+    lazy var priceValid: Observable<Bool> = {
+        return priceText
+            .map{ ($0 ?? "").count > 0}
+            .share(replay: 1)
+    }()
+
+    lazy var allFieldsValid: Observable<Bool> = {
+        return Observable
+            .combineLatest(nameValid, categoryValid, priceValid) { $0 && $1 && $2 }
+            .share(replay: 1)
+    }()
+
+    private let stateSubject = BehaviorRelay<MVVMViewSubmitState>(value: .idle)
+    var state: Driver<MVVMViewSubmitState> {
+        return stateSubject.asDriver(onErrorJustReturn: .idle)
+    }
+    let dismissSubject = BehaviorRelay<Bool>(value: false)
 
     // for viewController
     convenience init(viewController: UIViewController) {
@@ -24,5 +70,38 @@ class MVVMRegistViewModel {
     init(viewController: UIViewController, request: ItemAPIRequestRxProtocol) {
         self.viewController = viewController
         self.apiRequest = request
+    }
+
+    func bind() {
+    }
+
+    func postItem() -> Completable {
+        stateSubject.accept(.itemSubmitting)
+        guard let name = nameText.value, let category = categoryText.value,
+            let priceStr = priceText.value, let price = Int(priceStr) else {
+                self.stateSubject.accept(.errorOccurred(.invalidRequest))
+                self.stateSubject.accept(.idle)
+                return Completable.empty()
+        }
+        let item = ItemRx(id: nil, name: name, category: category, price: price)
+        return apiRequest.postItem(item: item)
+            .do(
+                onSuccess: { [weak self] _ in
+                    self?.stateSubject.accept(.itemSubmitCompleted)
+                    self?.stateSubject.accept(.idle)
+                    self?.dismissSubject.accept(true)
+                },
+                onError: { [weak self] error in
+                    print("Error: \(error)")
+                    self?.stateSubject.accept(.errorOccurred(.responseFailure))
+                    self?.stateSubject.accept(.idle)
+                }
+            )
+            .map { _ in }
+            .asCompletable()
+    }
+
+    func putItem() {
+        
     }
 }
