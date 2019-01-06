35b458829f1ecae0b5e810676a7c71b1b898df1d
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-06 14:40:13 +0900

    add testStateFetch

--- FunctionConfirmTests/MVVMListViewModelText.swift
+++ FunctionConfirmTests/MVVMListViewModelText.swift
@@ -7,6 +7,11 @@
 //
 
 import XCTest
+@testable import FunctionConfirm
+import RxSwift
+import RxCocoa
+import RxTest
+import RxBlocking
 
 class MVVMListViewModelText: XCTestCase {
 
@@ -18,11 +23,47 @@ class MVVMListViewModelText: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
+    func testStateFetch() {
+        let scheduler = TestScheduler(initialClock: 0)
+        let viewModel = MVVMListViewModel()
+        viewModel.apiRequest = MockItemAPIRequestRx()
+        let disposeBag = DisposeBag()
+        let results = scheduler.createObserver(MVVMViewFetchState.self)
+
+        scheduler.scheduleAt(100) {
+            viewModel.stateFetch
+                .drive(results)
+                .disposed(by: disposeBag)
+        }
+
+        scheduler.scheduleAt(200) {
+            _ = try? viewModel.fetchItems().toBlocking().single()
+        }
+
+        scheduler.start()
+
+        let expectedEvents = Recorded.events(
+            .next(100, MVVMViewFetchState.idle),
+            .next(200, MVVMViewFetchState.itemsFetching),
+            .next(200, MVVMViewFetchState.itemsFetchCompleted),
+            .next(200, MVVMViewFetchState.idle)
+        )
+        XCTAssertEqual(results.events, expectedEvents)
     }
 
     func testPerformanceExample() {
         self.measure {
+        }
+    }
+
+    class MockItemAPIRequestRx: ItemAPIRequestRx {
+        override func getItemsAPI() -> Single<GetItemResponseRx> {
+            let testData = GetItemResponseRx(status: 0, data: [
+                ItemRx(id: 1, name: "Apple", category: "Fruit", price: 100),
+                ItemRx(id: 2, name: "Orange", category: "Fruit", price: 200),
+                ItemRx(id: 3, name: "Banana", category: "Fruit", price: 300)
+                ])
+            return Observable.of(testData).asSingle()
         }
     }
 }
