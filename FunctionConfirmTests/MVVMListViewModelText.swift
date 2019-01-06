efa5c61ebf6ba2e978f49ddeb622331ca0c2c00b
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-06 13:59:27 +0900

    add MVVM test files

---
+++ FunctionConfirmTests/MVVMListViewModelText.swift
@@ -0,0 +1,28 @@
+//
+//  MVVMListViewModelText.swift
+//  FunctionConfirmTests
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/01/06.
+//  Copyright © 2019年 牧宥作. All rights reserved.
+//
+
+import XCTest
+
+class MVVMListViewModelText: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+}
