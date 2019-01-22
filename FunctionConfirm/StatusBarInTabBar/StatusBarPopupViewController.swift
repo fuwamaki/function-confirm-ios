06b55cf1b575393479bb3b22d3ca419cb41d736a
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-22 18:26:05 +0900

    add StatusBarPopupViewController.swift file

---
+++ FunctionConfirm/StatusBarInTabBar/StatusBarPopupViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StatusBarPopupViewController.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/01/22.
+//  Copyright © 2019年 牧宥作. All rights reserved.
+//
+
+import Foundation
