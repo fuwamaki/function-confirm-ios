c6550b38ee09850c7347d9669faf65321cb3401a
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-22 22:32:24 +0900

    add StatusBarThirdViewController

---
+++ FunctionConfirm/StatusBarInTabBar/StatusBarThirdViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StatusBarThirdViewController.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/01/22.
+//  Copyright © 2019年 牧宥作. All rights reserved.
+//
+
+import UIKit
+
+final class StatusBarThirdViewController: UIViewController {
+
+    private var statusBarStyle: UIStatusBarStyle = .default
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        setStatusBarStyle(style: .lightContent)
+//        let viewController = StatusBarPopupViewController(delegate: self)
+//        tabBarController?.present(viewController, animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return statusBarStyle
+    }
+
+    // StatusBarStyle
+    // MEMO: Info.plistの「View controller-based status bar appearance」をYESにしておく必要がある。
+    // MEMO: UINavigationControllerのextensionで、chilForStatusBarStyleを設定しておく必要がある。
+    func setStatusBarStyle(style: UIStatusBarStyle) {
+        statusBarStyle = style
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
+}
