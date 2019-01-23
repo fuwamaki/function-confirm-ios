a9041d3014b10a0b301db06dda4cb8a7d2c54bc8
fuwamaki <variety.fuwa.3@gmail.com>
2019-01-23 15:07:18 +0900

    fix StatusBarForthViewController

--- FunctionConfirm/StatusBarInTabBar/Forth/StatusBarForthViewController.swift
+++ FunctionConfirm/StatusBarInTabBar/Forth/StatusBarForthViewController.swift
@@ -13,15 +13,11 @@ import UIKit
 final class StatusBarForthViewController: UIViewController {
 
     @IBAction func buttonTapped(_ sender: Any) {
-        let viewController = StatusBarPopup2ViewController()
+        let viewController = StatusBarPopup3ViewController()
         tabBarController?.present(viewController, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return StatusBar2Manager.shared.statusBarStyle
     }
 }
