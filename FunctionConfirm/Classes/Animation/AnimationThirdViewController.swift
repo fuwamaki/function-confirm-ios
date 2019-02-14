36df8cdadf0d15dd7a50cbe0f55627c596d0294d
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-15 04:12:14 +0900

    add an animation on AnimationThird

--- FunctionConfirm/Classes/Animation/AnimationThirdViewController.swift
+++ FunctionConfirm/Classes/Animation/AnimationThirdViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+// StackViewのAnimationを試す用のクラス
 final class AnimationThirdViewController: UIViewController {
+
+    @IBOutlet private weak var purpleView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let redView = UIView(frame: purpleView.frame)
+        redView.backgroundColor = .red
+        UIView.transition(from: purpleView, to: redView, duration: 1.0, options: [.transitionCurlDown, .autoreverse], completion: { _ in
+            redView.removeFromSuperview()
+        })
     }
 }
