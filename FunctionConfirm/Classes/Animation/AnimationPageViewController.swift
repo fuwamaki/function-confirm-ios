8f5797c58c2432a84599c2252c476371d42b88b9
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-14 13:52:53 +0900

    add page control and fix update timing about navigation info

--- FunctionConfirm/Classes/Animation/AnimationPageViewController.swift
+++ FunctionConfirm/Classes/Animation/AnimationPageViewController.swift
@@ -10,31 +10,69 @@ import UIKit
 
 final class AnimationPageViewController: UIPageViewController {
 
+    private var pageControl: UIPageControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setViewControllers([getFirst()], direction: .forward, animated: true, completion: nil)
+        delegate = self
         dataSource = self
+        setupPageControl()
+        setupFirstViewController()
     }
 
-    func getFirst() -> UIViewController {
+    private func setupFirstViewController() {
+        let firstViewController = getFirst()
+        self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        updatePageInfo(currentViewController: firstViewController)
+    }
+
+    private func setupPageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0,
+                                                  y: self.view.frame.height - 100,
+                                                  width: self.view.frame.width,
+                                                  height: 50))
+        pageControl.backgroundColor = UIColor.baseGray
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        pageControl.pageIndicatorTintColor = UIColor.black
+        pageControl.numberOfPages = 3 // ページ数
+        pageControl.currentPage = 0
+        pageControl.isUserInteractionEnabled = false
+        view.addSubview(pageControl)
+    }
+
+    private func getFirst() -> UIViewController {
         let storyBoard = UIStoryboard(name: "Animation", bundle: nil)
         let viewController = storyBoard.instantiateViewController(withIdentifier: "AnimationFirstViewController")
-        navigationItem.title = "Animation First"
         return viewController
     }
 
-    func getSecond() -> UIViewController {
+    private func getSecond() -> UIViewController {
         let storyBoard = UIStoryboard(name: "Animation", bundle: nil)
         let viewController = storyBoard.instantiateViewController(withIdentifier: "AnimationSecondViewController")
-        navigationItem.title = "Animation Second"
         return viewController
     }
 
-    func getThird() -> UIViewController {
+    private func getThird() -> UIViewController {
         let storyBoard = UIStoryboard(name: "Animation", bundle: nil)
         let viewController = storyBoard.instantiateViewController(withIdentifier: "AnimationThirdViewController")
-        navigationItem.title = "Animation Third"
         return viewController
+    }
+
+    // ページ情報（navigationのタイトルとpageControl）を更新
+    private func updatePageInfo(currentViewController: UIViewController) {
+        switch currentViewController {
+        case is AnimationFirstViewController:
+            navigationItem.title = "Animation First"
+            pageControl.currentPage = 0
+        case is AnimationSecondViewController:
+            navigationItem.title = "Animation Second"
+            pageControl.currentPage = 1
+        case is AnimationThirdViewController:
+            navigationItem.title = "Animation Third"
+            pageControl.currentPage = 2
+        default:
+            break
+        }
     }
 }
 
@@ -57,5 +95,17 @@ extension AnimationPageViewController: UIPageViewControllerDataSource {
         } else {
             return nil
         }
+    }
+    // MEMO: 上記pageViewControllerメソッドでページ情報の更新するのは適さない。
+    // なぜならば、pageViewControllerは、[ページング開始時,ページング完了時]に呼ばれるメソッドだから。
+}
+
+extension AnimationPageViewController: UIPageViewControllerDelegate {
+    // ページングが完了したらページ情報を更新する
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard completed, let currentViewController = viewControllers?[0] else {
+            return
+        }
+        updatePageInfo(currentViewController: currentViewController)
     }
 }
