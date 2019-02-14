2a7107e3f622123992a55c3d9373454680a1d64b
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-15 00:41:06 +0900

    add block 1~12 in AnimationFirstViewController

--- FunctionConfirm/Classes/Animation/AnimationFirstViewController.swift
+++ FunctionConfirm/Classes/Animation/AnimationFirstViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 final class AnimationFirstViewController: UIViewController {
 
+    @IBOutlet private weak var block1Label: UILabel!
+    @IBOutlet private weak var block2Label: UILabel!
+    @IBOutlet private weak var block3Label: UILabel!
+    @IBOutlet private weak var block4Label: UILabel!
+    @IBOutlet private weak var block5Label: UILabel!
+    @IBOutlet private weak var block6Label: UILabel!
+    @IBOutlet private weak var block7Label: UILabel!
+    @IBOutlet private weak var block8Label: UILabel!
+    @IBOutlet private weak var block9Label: UILabel!
+    @IBOutlet private weak var block10Label: UILabel!
+    @IBOutlet private weak var block11Label: UILabel!
+    @IBOutlet private weak var block12Label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
