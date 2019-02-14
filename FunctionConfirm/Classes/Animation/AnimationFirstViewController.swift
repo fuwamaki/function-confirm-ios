5455349f77902195f18704f7bf5c0fccde00f261
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-15 04:11:11 +0900

    add some animation on AnimationFirst

--- FunctionConfirm/Classes/Animation/AnimationFirstViewController.swift
+++ FunctionConfirm/Classes/Animation/AnimationFirstViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// UIView.animateを試す用のクラス
 final class AnimationFirstViewController: UIViewController {
 
     @IBOutlet private weak var block1Label: UILabel!
@@ -25,10 +26,54 @@ final class AnimationFirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // viewDidLoadではanimation処理を置いてはいけない
+        // なぜならば、viewDidLoadはViewの生成をする箇所であり、Viewの生成と同時にアニメーションをしてはいけないから。
+        // 実際にAnimationによってはviewDidLoadで動作しないケースが存在する。
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startAnimating()
+    }
+
+    private func startAnimating() {
+        // memo - animateの引数について補足
+        // withDuration: 実行時間
+        // delay: 実行までの遅延
+
+        // memo - オプションの種類について
+        // .autoreverse: アニメーション逆再生
 
         // y軸に40下がった状態から、元の高さに上がって、また40下がる
         UIView.animate(withDuration: 1.0, delay: 0.0, options: .autoreverse, animations: {
             self.block1Label.center.y -= 40.0
         }, completion: nil)
+
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseIn, .autoreverse], animations: {
+            self.block2Label.center.y -= 40.0
+        }, completion: nil)
+
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseIn, .autoreverse], animations: {
+            self.block3Label.center.y -= 40.0
+        }, completion: { _ in
+            self.block3Label.center.y += 40.0
+        })
+
+        block4Label.alpha = 0.0
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseIn], animations: {
+            self.block4Label.alpha = 1.0
+        }, completion: nil)
+
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat], animations: {
+            self.block5Label.center.y -= 40.0
+        }, completion: nil)
+
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.0, options: .autoreverse, animations: {
+            self.block6Label.center.y -= 40.0
+            self.block6Label.bounds = CGRect(x: self.block6Label.center.x, y: self.block6Label.center.y, width: self.block6Label.bounds.width - 20, height: self.block6Label.bounds.height - 20)
+        }, completion: { _ in
+            self.block6Label.center.y += 40.0
+            self.block6Label.bounds = CGRect(x: self.block6Label.center.x, y: self.block6Label.center.y, width: self.block6Label.bounds.width + 20, height: self.block6Label.bounds.height + 20)
+        })
     }
 }
