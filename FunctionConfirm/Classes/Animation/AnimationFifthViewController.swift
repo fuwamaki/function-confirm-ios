9886fe57e7e8915a3b8f3f04ca91906c38c71b7d
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-17 22:47:12 +0900

    add animateKeyframes on animation fifth view controller

--- FunctionConfirm/Classes/Animation/AnimationFifthViewController.swift
+++ FunctionConfirm/Classes/Animation/AnimationFifthViewController.swift
@@ -15,4 +15,54 @@ final class AnimationFifthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // animateKeyframesを使えば、複数のアニメーションをセットできる
+        UIView.animateKeyframes(withDuration: 2.0, delay: 0.0, options: [.autoreverse], animations: {
+
+            // 100下に移動
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25, animations: {
+                self.blockLabel.center.y += 100.0
+            })
+
+            // 90度回転
+            // memo: Double.piは円周率
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+                self.blockLabel.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+            })
+
+            // 100右に移動
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.blockLabel.center.x += 100.0
+            })
+
+            // 180度回転
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.1, animations: {
+                self.blockLabel.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            })
+
+            // 100上に移動
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25, animations: {
+                self.blockLabel.center.y -= 100.0
+            })
+
+            // 270度回転
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.1, animations: {
+                self.blockLabel.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi + Double.pi / 2))
+            })
+
+            // 100左に移動
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                self.blockLabel.center.x -= 100.0
+            })
+
+            // 360度回転
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.1, animations: {
+                self.blockLabel.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2.0))
+            })
+
+        }, completion: nil)
+    }
 }
