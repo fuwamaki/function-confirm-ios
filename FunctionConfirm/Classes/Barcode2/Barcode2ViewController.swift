b9da48f21877d4e8b9672547255c96d34b80a8cf
fuwamaki <variety.fuwa.3@gmail.com>
2019-03-30 15:14:08 +0900

    add barcode2 view controller and working!

---
+++ FunctionConfirm/Classes/Barcode2/Barcode2ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  Barcode2ViewController.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/03/30.
+//  Copyright © 2019年 牧宥作. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class Barcode2ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var label: UILabel!
+
+    let detectionArea = UIView()
+    var timer: Timer!
+    var counter = 0
+    var isDetected = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // セッションのインスタンス生成
+        let captureSession = AVCaptureSession()
+
+        // 入力（背面カメラ）
+        let videoDevice = AVCaptureDevice.default(for: .video)
+        let videoInput = try! AVCaptureDeviceInput.init(device: videoDevice!)
+        captureSession.addInput(videoInput)
+
+        // 出力（ビデオデータ）
+        let metadataOutput = AVCaptureMetadataOutput()
+        captureSession.addOutput(metadataOutput)
+
+        // メタデータを検出した際のデリゲート設定
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        // EAN-13コードの認識を設定
+        metadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.ean13, AVMetadataObject.ObjectType.ean8]
+
+        // 検出エリアのビュー
+        let x: CGFloat = 0.05
+        let y: CGFloat = 0.3
+        let width: CGFloat = 0.9
+        let height: CGFloat = 0.2
+
+        detectionArea.frame = CGRect(x: view.frame.size.width * x, y: view.frame.size.height * y, width: view.frame.size.width * width, height: view.frame.size.height * height)
+        detectionArea.layer.borderColor = UIColor.red.cgColor
+        detectionArea.layer.borderWidth = 3
+        view.addSubview(detectionArea)
+
+        // 検出エリアの設定
+        metadataOutput.rectOfInterest = CGRect(x: y, y: 1-x-width, width: height, height: width)
+
+        // プレビュー
+        // if letとった。
+        let videoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
+        videoLayer.frame = previewView.bounds
+        videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewView.layer.addSublayer(videoLayer)
+
+        // セッションの開始
+        DispatchQueue.global(qos: .userInitiated).async {
+            captureSession.startRunning()
+        }
+
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // 複数のメタデータを検出できる
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+            // EAN-13Qコードのデータかどうかの確認
+            if metadata.type == AVMetadataObject.ObjectType.ean13 || metadata.type == AVMetadataObject.ObjectType.ean8 {
+                if metadata.stringValue != nil {
+                    // 検出データを取得
+                    counter = 0
+                    if !isDetected || label.text != metadata.stringValue! {
+                        isDetected = true
+                        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // バイブレーション
+                        label.text = metadata.stringValue!
+                        detectionArea.layer.borderColor = UIColor.white.cgColor
+                        detectionArea.layer.borderWidth = 5
+                    }
+                }
+            }
+        }
+    }
+
+//    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+//        // 複数のメタデータを検出できる
+//        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+//            // EAN-13Qコードのデータかどうかの確認
+//            if metadata.type == AVMetadataObject.ObjectType.ean13 || metadata.type == AVMetadataObject.ObjectType.ean8 {
+//                if metadata.stringValue != nil {
+//                    // 検出データを取得
+//                    counter = 0
+//                    if !isDetected || label.text != metadata.stringValue! {
+//                        isDetected = true
+//                        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // バイブレーション
+//                        label.text = metadata.stringValue!
+//                        detectionArea.layer.borderColor = UIColor.white.cgColor
+//                        detectionArea.layer.borderWidth = 5
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    @objc func update(tm: Timer) {
+        counter += 1
+        print(counter)
+        if 1 < counter {
+            detectionArea.layer.borderColor = UIColor.red.cgColor
+            detectionArea.layer.borderWidth = 3
+            label.text = ""
+        }
+    }
+}
--- FunctionConfirm/Main/MainViewController.swift
+++ FunctionConfirm/Main/MainViewController.swift
@@ -25,7 +25,7 @@ extension MainViewController: UITableViewDataSource {
 
     // セルを追加する場合の作業2/4: Section数を追加
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return 16
     }
 
     // セルを追加する場合の作業3/4: Cellを追加
@@ -88,9 +88,13 @@ extension MainViewController: UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: "awsRekognitionCell", for: indexPath)
             cell.textLabel?.text = "AWS顔認識"
             return cell
-        } else {
+        } else if indexPath.row == 14 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "barcodeCell", for: indexPath)
             cell.textLabel?.text = "バーコード"
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "barcode2Cell", for: indexPath)
+            cell.textLabel?.text = "バーコード2"
             return cell
         }
     }
@@ -159,9 +163,13 @@ extension MainViewController: UITableViewDelegate {
             let storyBoard = UIStoryboard(name: "AWSRekognition", bundle: nil)
             let viewController = storyBoard.instantiateViewController(withIdentifier: "FaceTrackingHomeViewController")
             navigationController?.pushViewController(viewController, animated: true)
-        default:
+        case 14:
             let storyBoard = UIStoryboard(name: "Barcode", bundle: nil)
             let viewController = storyBoard.instantiateViewController(withIdentifier: "BarcodeViewController")
+            navigationController?.pushViewController(viewController, animated: true)
+        default:
+            let storyBoard = UIStoryboard(name: "Barcode2", bundle: nil)
+            let viewController = storyBoard.instantiateViewController(withIdentifier: "Barcode2ViewController")
             navigationController?.pushViewController(viewController, animated: true)
         }
     }
