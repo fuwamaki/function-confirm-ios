72b9b085cfc12812d06c7959307d19048c5aa21d
fuwamaki <variety.fuwa.3@gmail.com>
2019-03-31 17:34:48 +0900

    change frame

--- FunctionConfirm/Classes/Barcode2/Barcode2ViewController.swift
+++ FunctionConfirm/Classes/Barcode2/Barcode2ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 import AVFoundation
+import SafariServices
 
-class Barcode2ViewController: UIViewController {
+final class Barcode2ViewController: UIViewController {
 
-    @IBOutlet private weak var previewView: UIView!
-    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var detectionAreaStackView: UIStackView!
+    @IBOutlet private weak var cameraPreviewView: UIView!
+    @IBOutlet private weak var detectionAreaView: UIView!
+    @IBOutlet private weak var resultTextLabel: UILabel!
+    @IBOutlet private weak var resultUrlStackView: UIStackView!
+
+    @IBAction private func clickWebPageButton(_ sender: Any) {
+        if let url = webPageUrl {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
 
     // セッションのインスタンス
     private let captureSession = AVCaptureSession()
@@ -23,8 +34,8 @@ class Barcode2ViewController: UIViewController {
     // ビデオデータ出力
     private let metadataOutput = AVCaptureMetadataOutput()
 
-    let detectionArea = UIView()
     var isDetected = false
+    var webPageUrl: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +44,8 @@ class Barcode2ViewController: UIViewController {
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        detectionAreaView.layer.borderColor = UIColor.green.cgColor
+        detectionAreaView.layer.borderWidth = 3
     }
 
     private func setupBarcodeCapture() {
@@ -47,25 +59,19 @@ class Barcode2ViewController: UIViewController {
         // EAN-13コードの認識を設定
         metadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.ean13, AVMetadataObject.ObjectType.ean8]
 
-        // 検出エリアのビュー
-        let x: CGFloat = 0.05
-        let y: CGFloat = 0.3
-        let width: CGFloat = 0.9
-        let height: CGFloat = 0.2
-
-        detectionArea.frame = CGRect(x: view.frame.size.width * x, y: view.frame.size.height * y, width: view.frame.size.width * width, height: view.frame.size.height * height)
-        detectionArea.layer.borderColor = UIColor.red.cgColor
-        detectionArea.layer.borderWidth = 3
-        view.addSubview(detectionArea)
-
         // 検出エリアの設定
+        // memo: 長さではダメだったので、小数値で設定
+        let x = (detectionAreaStackView.frame.origin.x + detectionAreaView.frame.origin.x)/cameraPreviewView.bounds.width
+        let y = (detectionAreaStackView.frame.origin.y + detectionAreaView.frame.origin.y)/cameraPreviewView.bounds.height
+        let width = (detectionAreaView.bounds.width)/cameraPreviewView.bounds.width
+        let height = (detectionAreaView.bounds.height)/cameraPreviewView.bounds.height
         metadataOutput.rectOfInterest = CGRect(x: y, y: 1-x-width, width: height, height: width)
 
         // プレビュー
         let videoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
-        videoLayer.frame = previewView.bounds
+        videoLayer.frame = cameraPreviewView.bounds
         videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        previewView.layer.addSublayer(videoLayer)
+        cameraPreviewView.layer.addSublayer(videoLayer)
 
         // セッションの開始
         DispatchQueue.global(qos: .userInitiated).async {
@@ -83,12 +89,12 @@ extension Barcode2ViewController: AVCaptureMetadataOutputObjectsDelegate {
             if metadata.type == AVMetadataObject.ObjectType.ean13 || metadata.type == AVMetadataObject.ObjectType.ean8 {
                 if metadata.stringValue != nil {
                     // 検出データを取得
-                    if !isDetected || label.text != metadata.stringValue! {
+                    if !isDetected || resultTextLabel.text != metadata.stringValue! {
                         isDetected = true
                         AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // バイブレーション
-                        label.text = metadata.stringValue!
-                        detectionArea.layer.borderColor = UIColor.white.cgColor
-                        detectionArea.layer.borderWidth = 5
+                        resultTextLabel.text = metadata.stringValue!
+                        detectionAreaView.layer.borderColor = UIColor.white.cgColor
+                        detectionAreaView.layer.borderWidth = 5
                     }
                 }
             }
