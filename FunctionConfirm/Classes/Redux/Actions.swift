898d4ac912bfa702d20e53548727f7bb2e9d5dd5
fuwamaki <variety.fuwa.3@gmail.com>
2019-04-28 00:59:45 +0900

    create redux sample system

---
+++ FunctionConfirm/Classes/Redux/Actions.swift
@@ -0,0 +1,13 @@
+//
+//  Actions.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/04/27.
+//  Copyright © 2019年 牧宥作. All rights reserved.
+//
+
+import ReSwift
+
+// all of the actions that can be applied to the state
+struct CounterActionIncrease: Action {}
+struct CounterActionDecrease: Action {}
--- FunctionConfirm/Classes/Redux/ReViewController.swift
+++ FunctionConfirm/Classes/Redux/ReViewController.swift
@@ -7,10 +7,32 @@
 //
 
 import UIKit
+import ReSwift
 
 final class ReViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+
+    @IBAction func clickUpButton(_ sender: Any) {
+        mainStore.dispatch(CounterActionIncrease())
+    }
+
+    @IBAction func clickDownButton(_ sender: Any) {
+        mainStore.dispatch(CounterActionDecrease())
+    }
+
+    typealias StoreSubscriberStateType = AppState
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // subscribe to state changes
+        mainStore.subscribe(self)
+    }
+
+    func newState(state: AppState) {
+        // when the state changes, the UI is updated to reflect the current state
+        label.text = "\(mainStore.state.counter)"
     }
 }
+
+extension ReViewController: StoreSubscriber {}
