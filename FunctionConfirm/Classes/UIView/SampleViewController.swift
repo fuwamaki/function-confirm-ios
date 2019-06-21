16a6b2e1bb76e3c18cc4925f108d00dacb1346ce
fuwamaki <variety.fuwa.3@gmail.com>
2019-06-21 15:22:01 +0900

    add sample view controller

---
+++ FunctionConfirm/Classes/UIView/SampleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SampleViewController.swift
+//  FunctionConfirm
+//
+//  Created by Maki, Yusaku | Mackey | ECID on 2019/06/21.
+//  Copyright © 2019 牧宥作. All rights reserved.
+//
+
+import UIKit
+
+final class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
