77aaac1e12c904d2087541ef704454829e54b3e7
fuwamaki <variety.fuwa.3@gmail.com>
2019-06-21 15:24:26 +0900

    add view life cycle of viewcontroller

--- FunctionConfirm/Classes/UIView/SampleViewController.swift
+++ FunctionConfirm/Classes/UIView/SampleViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 
 final class SampleViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 }
