ce4c9ec74df887e9e23164976c336db66db47741
fuwamaki <yusaku.maki@street-academy.com>
2019-10-03 13:12:29 +0900

    fix RoundedCornerButton

--- FunctionConfirm/Classes/RoundedCorner/RoundedCornerButton.swift
+++ FunctionConfirm/Classes/RoundedCorner/RoundedCornerButton.swift
@@ -15,12 +15,13 @@ final class RoundedCornerButton: UIButton {
     @IBInspectable var iconImage: UIImage = UIImage()
 
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
         stackView.translatesAutoresizingMaskIntoConstraints = false // autoLayoutをONに
         stackView.alignment = .center
         stackView.distribution = .fill
         stackView.axis = .horizontal
         stackView.spacing = 4.0
+        stackView.backgroundColor = UIColor.clear
         return stackView
     }()
 
@@ -32,8 +33,10 @@ final class RoundedCornerButton: UIButton {
     }()
 
     private lazy var textLabel: UILabel = {
-        let textLabel = UILabel()
+        let width = stackView.frame.width - iconImageView.frame.width
+        let textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: frame.height))
         textLabel.text = text
+        textLabel.textAlignment = .center
         textLabel.textColor = UIColor(hex: "#4A4A4A")
         textLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
         return textLabel
@@ -57,6 +60,8 @@ final class RoundedCornerButton: UIButton {
         layer.cornerRadius = 4.0
         layer.shadowOpacity = 1.0
         layer.shadowRadius = 0.0
+        layer.backgroundColor = UIColor(hex: "#FFFFFF").cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
 
         self.addSubview(stackView)
         stackView.addArrangedSubview(iconImageView)
@@ -65,50 +70,22 @@ final class RoundedCornerButton: UIButton {
         stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
 
         [UIControl.State.normal, UIControl.State.highlighted, UIControl.State.selected].forEach { state in
-            switch state {
-            case .normal:
-                backgroundColor = UIColor(hex: "#FFFFFF")
-                layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-            case .highlighted:
-                layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-            case .selected:
-                backgroundColor = UIColor(hex: "#EDEDED")
-                layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-            default:
-                break
-            }
+            let image = backgroundImage(for: state)
+            setBackgroundImage(image, for: state)
         }
     }
 
-//    override func backgroundImage(for state: UIControl.State) -> UIImage? {
-//        switch state {
-//        // style: .filled
-//        case .highlighted:
-//            return loadBackgroundImage(name: "button_style_filled", opacity: Constant.highlightOpacity)
-//        case .disabled:
-//            return loadBackgroundImage(name: "button_style_filled", tint: ColorPalette.backgroundGray)
-//        default:
-//            return loadBackgroundImage(name: "button_style_filled", tint: tintColor)
-//        }
-//    }
-//
-//    private func loadBackgroundImage(name: String, opacity: CGFloat? = nil, tint color: UIColor? = nil) -> UIImage? {
-//        var image = UIImage(named: name, in: Bundle(for: type(of: self)), compatibleWith: traitCollection)
-//        if let opacity = opacity {
-//            image = image?.withOpacity(opacity: opacity)
-//        }
-//        if let color = color {
-//            image = image?.tinted(color: color)
-//        }
-//        return image?.resizableImage(withCapInsets: Constant.backgroundInsets)
-//    }
-//
-//    override func titleColor(for state: UIControl.State) -> UIColor {
-//        switch state {
-//        case .highlighted:
-//            return ColorPalette.textWhite.withAlphaComponent(Constant.highlightOpacity)
-//        default:
-//            return ColorPalette.textWhite
-//        }
-//    }
+    override func backgroundImage(for state: UIControl.State) -> UIImage? {
+        switch state {
+        case .normal:
+//            layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+            return UIColor(hex: "#FFFFFF").image
+        case .highlighted:
+//            layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+            return UIColor(hex: "#000000").image
+        default:
+//            layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+            return UIColor(hex: "#000000").image
+        }
+    }
 }
