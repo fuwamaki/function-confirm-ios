cb868ab62aaec851e47009f817c7ebbc93752035
fuwamaki <yusaku.maki@street-academy.com>
2019-10-05 12:00:52 +0900

    add heart animation

--- FunctionConfirm/Classes/RoundedCorner/ImageButton.swift
+++ FunctionConfirm/Classes/RoundedCorner/ImageButton.swift
@@ -31,8 +31,41 @@ final class ImageButton: UIButton {
         setNeedsDisplay()
     }
 
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                if imageView?.layer.animation(forKey: "reduced-size") == nil {
+                    let animation = CASpringAnimation(keyPath: "transform.scale")
+                    animation.duration = 0.05 // animation時間
+                    animation.fromValue = 1.0 // animation前サイズ
+                    animation.toValue = 0.95 // animation後サイズ
+                    animation.mass = 0.1 // 質量
+                    animation.autoreverses = false // 自動でfromの値に戻らない
+                    animation.initialVelocity = 40.0 // 初速度
+                    animation.damping = 1.0 // 硬さ
+                    animation.stiffness = 40.0 // バネの弾性力
+                    animation.isRemovedOnCompletion = false // animation動作後に完了状態としない
+                    animation.fillMode = .forwards // 一方向モード。fromの形状に戻らない
+                    imageView?.layer.add(animation, forKey: "reduced-size")
+                }
+            }
+        }
+    }
+
     private func setupImageView() {
-        setImage(selectedStatus ? selectedImage : unselectedImage, for: .normal)
-        setImage(selectedStatus ? selectedImage : unselectedImage, for: .highlighted)
+        self.setImage(self.selectedStatus ? self.selectedImage : self.unselectedImage, for: .normal)
+        self.setImage(self.selectedStatus ? self.selectedImage : self.unselectedImage, for: .highlighted)
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.duration = 0.3 // 初速度
+        animation.fromValue = 0.95 // animation前サイズ
+        animation.toValue = 1.0 // animation前サイズ
+        animation.mass = 0.6 // 質量
+        animation.initialVelocity = 40.0 // 初速度
+        animation.damping = 3.0 // 硬さ
+        animation.stiffness = 40.0 // バネの弾性力
+        imageView?.layer.add(animation, forKey: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.imageView?.layer.removeAllAnimations() // highlightのAnimationも含め、removeする
+        }
     }
 }
