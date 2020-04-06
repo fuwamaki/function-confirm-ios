e3e6b6ac97922121c05c9204a9a144d48f3ca1bc
fuwamaki <yusaku.maki@street-academy.com>
2020-04-06 13:36:22 +0900

    put modalPresent files in order

---
+++ FunctionConfirm/Classes/SemiModal/DismissAnimator.swift
@@ -0,0 +1,36 @@
+//
+//  DismissAnimator.swift
+//  FunctionConfirm
+//
+//  Created by yusaku maki on 2020/04/06.
+//  Copyright © 2020 牧宥作. All rights reserved.
+//
+
+import UIKit
+
+class DismissAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.4
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let fromVC = transitionContext.viewController(forKey: .from) else { return }
+        let containerView = transitionContext.containerView
+        containerView.addSubview(fromVC.view)
+        let screenBounds = UIScreen.main.bounds
+        let bottomLeftCorner = CGPoint(x: 0, y: screenBounds.height)
+        let finalFrame = CGRect(origin: bottomLeftCorner, size: screenBounds.size)
+        let option: UIView.AnimationOptions = transitionContext.isInteractive ? .curveLinear : .curveEaseIn
+
+        UIView.animate(withDuration: transitionDuration(using: transitionContext),
+                       delay: 0,
+                       options: [option],
+                       animations: {
+                           fromVC.view.frame = finalFrame
+                       },
+                       completion: { _ in
+                           transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+                       }
+        )
+    }
+}
