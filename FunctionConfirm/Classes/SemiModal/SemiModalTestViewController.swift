fcb811622be9df1b5ae4890e87d095a4c6e1b891
fuwamaki <yusaku.maki@street-academy.com>
2020-04-06 14:20:29 +0900

    delete OverCurrentTransitionable

--- FunctionConfirm/Classes/SemiModal/SemiModalTestViewController.swift
+++ FunctionConfirm/Classes/SemiModal/SemiModalTestViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class SemiModalTestViewController: UIViewController, OverCurrentTransitionable {
+final class SemiModalTestViewController: UIViewController {
 
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var headerView: UIView!
@@ -75,6 +75,51 @@ final class SemiModalTestViewController: UIViewController, OverCurrentTransition
         /// インタラクション開始位置と、テーブルビュースクロール開始位置が異なるため、インタラクション開始時のY位置を取得している
         interactor.setStartInteractionTranslationY(sender.translation(in: view).y)
         handleTransitionGesture(sender)
+    }
+
+    private func handleTransitionGesture(_ sender: UIPanGestureRecognizer) {
+        ///　TableViewからPanGestureを取得する場合、dismiss開始をsender.state.beganで判断できないため、Interactor.stateで判定している
+        switch interactor.state {
+        case .shouldStart:
+            interactor.state = .hasStarted
+            dismiss(animated: true, completion: nil)
+        case .hasStarted, .shouldFinish:
+            break
+        case .none:
+            return
+        }
+
+        /// セミモーダルが画面の何割移動したかを計算
+        let translation = sender.translation(in: view)
+        let verticalMovement = (translation.y - interactor.startInteractionTranslationY) / view.bounds.height
+        let downwardMovement = fmaxf(Float(verticalMovement), 0.0)
+        let downwardMovementPercent = fminf(downwardMovement, 1.0)
+        let progress = CGFloat(downwardMovementPercent)
+
+        /// PanGesture.stateごとに、インタラクションの更新、終了、キャンセルを制御
+        switch sender.state {
+        case .changed:
+            /// スクロール量がしきい値を超えたか？　もしくは　スクロール速度がしきい値を超えたか？
+            if progress > 0.3 || sender.velocity(in: view).y > 1200 {
+                interactor.state =  .shouldFinish
+            } else {
+                interactor.state =  .hasStarted
+            }
+            interactor.update(progress)
+        case .cancelled:
+            interactor.cancel()
+            interactor.reset()
+        case .ended:
+            switch interactor.state {
+            case .shouldFinish:
+                interactor.finish()
+            default:
+                interactor.cancel()
+            }
+            interactor.reset()
+        default:
+            break
+        }
     }
 }
 
