a3dc5ef5c3169258c319caf88db19f4728e3fc6e
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-06-18 16:56:05 +0900

    write medium view

--- FunctionConfirm/Classes/MediumModal/MediumRootViewController.swift
+++ FunctionConfirm/Classes/MediumModal/MediumRootViewController.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import PhotosUI
 
 final class MediumRootViewController: UIViewController {
+
+    @IBAction func clickButton(_ sender: Any) {
+        showImagePicker()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // TODO: Xcode13 betaでは実装できないよう。更新まで待つ。
+    func showImagePicker() {
+        let configuration = PHPickerConfiguration()
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+//        picker.sheet
+//        if let sheet = picker.sheetPresentationController {
+//            sheet.detents = [.medium(), .large()]
+//        }
+        present(picker, animated: true)
+    }
+}
+
+// MARK: PHPickerViewControllerDelegate
+extension MediumRootViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true, completion: nil)
     }
 }
