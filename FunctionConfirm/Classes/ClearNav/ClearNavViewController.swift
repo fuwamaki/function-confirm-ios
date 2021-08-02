eb9860896d67468a28950ce775475fe8a400b5b7
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-08-02 17:13:21 +0900

    create back button

--- FunctionConfirm/Classes/ClearNav/ClearNavViewController.swift
+++ FunctionConfirm/Classes/ClearNav/ClearNavViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 final class ClearNavViewController: UIViewController {
 
+    private lazy var backBarButtonItem: UIBarButtonItem = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "blue_back")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        button.addTarget(self, action: #selector(clickBack), for: .touchUpInside)
+        return UIBarButtonItem(customView: button)
+    }()
+
     private lazy var favoriteBarButtonItem: UIBarButtonItem = {
         let button = UIButton(type: .custom)
         button.setImage(UIImage(named: "blue_star")?.withRenderingMode(.alwaysOriginal), for: .normal)
         button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        button.addTarget(self, action: #selector(clickFavorite), for: .touchUpInside)
         return UIBarButtonItem(customView: button)
     }()
 
@@ -21,13 +30,27 @@ final class ClearNavViewController: UIViewController {
         let button = UIButton(type: .custom)
         button.setImage(UIImage(named: "blue_triangle")?.withRenderingMode(.alwaysOriginal), for: .normal)
         button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        button.addTarget(self, action: #selector(clickTriangle), for: .touchUpInside)
         return UIBarButtonItem(customView: button)
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.setRightBarButtonItems([triangleBarButtonItem, favoriteBarButtonItem], animated: false)
+        navigationItem.setLeftBarButtonItems([backBarButtonItem], animated: false)
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    @objc private func clickBack() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc private func clickFavorite() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc private func clickTriangle() {
+        navigationController?.popViewController(animated: true)
     }
 }
---
+++ FunctionConfirm/Extension/UIViewController+Addition.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Addition.swift
+//  FunctionConfirm
+//
+//  Created by yusaku maki on 2021/08/02.
+//  Copyright © 2021 fuwamaki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//extension UIViewController {
+//    class func back() {
+//        
+//    }
+//}
