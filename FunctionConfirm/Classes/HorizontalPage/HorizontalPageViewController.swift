828921afac57d948b71469301d7bed8de0ea1973
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-08-20 16:10:24 +0900

    realize HorizontalPage View

--- FunctionConfirm/Classes/HorizontalPage/HorizontalPageViewController.swift
+++ FunctionConfirm/Classes/HorizontalPage/HorizontalPageViewController.swift
@@ -10,10 +10,55 @@ import UIKit
 
 final class HorizontalPageViewController: UIViewController {
 
-    @IBOutlet private weak var sampleScrollView: UIScrollView!
-    @IBOutlet private weak var samplePageControl: UIPageControl!
+    @IBOutlet private weak var sampleScrollView: UIScrollView! {
+        didSet {
+            sampleScrollView.delegate = self
+            sampleScrollView.isPagingEnabled = true
+            sampleScrollView.showsHorizontalScrollIndicator = false
+        }
+    }
+    @IBOutlet private weak var samplePageControl: UIPageControl! {
+        didSet {
+            samplePageControl.isUserInteractionEnabled = false
+        }
+    }
+
+    private lazy var images: [UIImage] = {
+        return [UIImage(named: "img_entertainment")!,
+                UIImage(named: "img_service")!,
+                UIImage(named: "img_technology")!]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCoverImages()
+    }
+
+    private func setupCoverImages() {
+        sampleScrollView.subviews.forEach {
+            $0.removeFromSuperview()
+        }
+        images.enumerated().forEach { index, image in
+            let imageView = UIImageView(
+                frame: CGRect(
+                    x: UIScreen.main.bounds.width * CGFloat(index),
+                    y: 0,
+                    width: UIScreen.main.bounds.width,
+                    height: 200))
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFill
+            sampleScrollView.addSubview(imageView)
+        }
+        sampleScrollView.contentSize = CGSize(
+            width: UIScreen.main.bounds.width * CGFloat(images.count),
+            height: 200)
+        samplePageControl.numberOfPages = images.count
+    }
+}
+
+// MARK: UIScrollViewDelegate
+extension HorizontalPageViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        samplePageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width)
     }
 }
