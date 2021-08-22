b97a8f790b0eef1b173e8929cc105eb747715c09
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-08-22 20:45:48 +0900

    completed HorizontalPageView

--- FunctionConfirm/Classes/HorizontalPage/HorizontalPageViewController.swift
+++ FunctionConfirm/Classes/HorizontalPage/HorizontalPageViewController.swift
@@ -13,7 +13,7 @@ final class HorizontalPageViewController: UIViewController {
     @IBOutlet private weak var sampleScrollView: UIScrollView! {
         didSet {
             sampleScrollView.delegate = self
-            sampleScrollView.isPagingEnabled = true
+            sampleScrollView.isPagingEnabled = false
             sampleScrollView.showsHorizontalScrollIndicator = false
         }
     }
@@ -24,7 +24,21 @@ final class HorizontalPageViewController: UIViewController {
     }
 
     private let scrollHeight: CGFloat = 200.0
-    private let imageWidth: CGFloat = UIScreen.main.bounds.width
+
+    // 飛び出して見えている画像の横幅
+    private let protrudingWidth: CGFloat = 16.0
+    // 画像の間隔
+    private let spaceBetweenImages: CGFloat = 16.0
+
+    // 画像の横幅
+    private var imageWidth: CGFloat {
+        return UIScreen.main.bounds.width - ((protrudingWidth + spaceBetweenImages) * 2)
+    }
+
+    // ページの横幅
+    private var pageWidth: CGFloat {
+        return imageWidth + spaceBetweenImages
+    }
 
     private var fixedImages: [UIImage] {
         return [UIImage(named: "img_entertainment")!,
@@ -45,7 +59,9 @@ final class HorizontalPageViewController: UIViewController {
             $0.removeFromSuperview()
         }
         imageList.enumerated().forEach { index, image in
-            let imageView = UIImageView(frame: CGRect(x: imageWidth * CGFloat(index),
+            let imageView = UIImageView(frame: CGRect(x: pageWidth * CGFloat(index)
+                                                      + protrudingWidth
+                                                      + spaceBetweenImages,
                                                       y: 0,
                                                       width: imageWidth,
                                                       height: scrollHeight))
@@ -53,7 +69,7 @@ final class HorizontalPageViewController: UIViewController {
             imageView.contentMode = .scaleAspectFill
             sampleScrollView.addSubview(imageView)
         }
-        sampleScrollView.contentSize = CGSize(width: imageWidth * CGFloat(imageList.count),
+        sampleScrollView.contentSize = CGSize(width: pageWidth * CGFloat(imageList.count),
                                               height: scrollHeight)
         samplePageControl.numberOfPages = fixedImages.count
     }
@@ -61,30 +77,56 @@ final class HorizontalPageViewController: UIViewController {
 
 // MARK: UIScrollViewDelegate
 extension HorizontalPageViewController: UIScrollViewDelegate {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView,
+                                   withVelocity velocity: CGPoint,
+                                   targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        // ドラッグを離した時の、ScrollView内における左端位置
+        let positionXOfScroll: CGFloat = scrollView.contentOffset.x
+        // 各Pageの、ScrollView内における左端位置の配列
+        let positionXOfPages: [CGFloat] = scrollView.subviews
+            .map { $0.frame.minX - (protrudingWidth + spaceBetweenImages) }
+        // ドラッグを離した時に、自動スクロールセットしたいPageの左端位置
+        let targetPositionX = positionXOfPages
+            .min(by: { abs($0 - positionXOfScroll) <= abs($1 - positionXOfScroll) })!
+        if abs(velocity.x) < 0.2 {
+            // 勢いがないスクロールなら、targetPositionXに
+            targetContentOffset.pointee.x = targetPositionX
+        } else if velocity.x < 0,
+                  let positionX = positionXOfPages.filter({ $0 < positionXOfScroll }).max() {
+            // 左方向への勢いがあれば、左側のPageに
+            // memo: 0だと正常に機能しないので、0に近くて0ではない値に
+            targetContentOffset.pointee.x = (positionX == 0) ? 1 : positionX
+        } else if velocity.x > 0,
+                  let positionX = positionXOfPages.filter({ $0 > positionXOfScroll }).min() {
+            // 右方向への勢いがあれば、右側のPageに
+            targetContentOffset.pointee.x = positionX
+        }
+    }
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.x > imageWidth * 1.5 {
+        if scrollView.contentOffset.x > pageWidth * 1.5 {
             // 先頭要素を末尾に
-            if let first = self.imageList.first {
-                self.imageList.append(first)
-                self.imageList.removeFirst()
+            if let first = imageList.first {
+                imageList.append(first)
+                imageList.removeFirst()
             }
             // 再描画
             setupImages()
             // contentOffsetの調整
-            self.sampleScrollView.contentOffset.x -= imageWidth
+            sampleScrollView.contentOffset.x -= pageWidth
             // pageControlのcurrent更新
             samplePageControl.currentPage = fixedImages.firstIndex(of: imageList[1])!
         }
-        if sampleScrollView.contentOffset.x < imageWidth * 0.5 {
+        if sampleScrollView.contentOffset.x < pageWidth * 0.5 {
             // 末尾要素を先頭に
-            if let last = self.imageList.last {
-                self.imageList.insert(last, at: 0)
-                self.imageList.removeLast()
+            if let last = imageList.last {
+                imageList.insert(last, at: 0)
+                imageList.removeLast()
             }
             // 再描画
             setupImages()
             // contentOffsetの調整
-            self.sampleScrollView.contentOffset.x += imageWidth
+            sampleScrollView.contentOffset.x += pageWidth
             // pageControlのcurrent更新
             samplePageControl.currentPage = fixedImages.firstIndex(of: imageList[1])!
         }
