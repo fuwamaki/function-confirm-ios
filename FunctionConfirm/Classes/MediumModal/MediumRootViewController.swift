cdd106a4bd45c3b7d5ad5bd5b5e41c1f9dcf529a
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-09-18 09:02:17 +0900

    fix medium root view

--- FunctionConfirm/Classes/MediumModal/MediumRootViewController.swift
+++ FunctionConfirm/Classes/MediumModal/MediumRootViewController.swift
@@ -11,10 +11,13 @@ import PhotosUI
 
 final class MediumRootViewController: UIViewController {
 
+    @IBOutlet private weak var imageView: UIImageView!
+
     @IBAction func clickButton(_ sender: Any) {
         if let sheet = phpickerViewController.sheetPresentationController {
             sheet.detents = [.medium(), .large()]
             sheet.prefersGrabberVisible = true
+            sheet.largestUndimmedDetentIdentifier = .medium
         }
         present(phpickerViewController, animated: true)
     }
@@ -34,6 +37,16 @@ final class MediumRootViewController: UIViewController {
 // MARK: PHPickerViewControllerDelegate
 extension MediumRootViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        dismiss(animated: true, completion: nil)
+        results.forEach { result in
+            result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] item, error in
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                } else if let image = item as? UIImage {
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
     }
 }
