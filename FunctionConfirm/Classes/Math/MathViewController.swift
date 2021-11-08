5db8af81c9909a0ec1b8716360b270d928655ebf
fuwamaki <variety.fuwa.3@gmail.com>
2021-11-08 09:38:32 +0900

    show numerical formula

--- FunctionConfirm/Classes/Math/MathViewController.swift
+++ FunctionConfirm/Classes/Math/MathViewController.swift
@@ -7,10 +7,17 @@
 //
 
 import UIKit
+import iosMath
 
 final class MathViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let label: MTMathUILabel = MTMathUILabel(
+            frame: CGRect(x: 10, y: 280, width: 330, height: 80))
+        label.fontSize = 18
+//        label.latex = "x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}"
+        label.latex = "\\cos(\\theta + \\varphi) = \\cos(\\theta)\\cos(\\varphi) - \\sin^2(\\theta)\\sin(\\varphi)"
+        view.addSubview(label)
     }
 }
