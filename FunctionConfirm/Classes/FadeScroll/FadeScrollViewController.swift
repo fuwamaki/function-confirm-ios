95b9e573638d6b3941b5c9aae019c893f945039d
fuwamaki <variety.fuwa.3@gmail.com>
2022-05-06 09:50:29 +0900

    realize fade scroll

--- FunctionConfirm/Classes/FadeScroll/FadeScrollViewController.swift
+++ FunctionConfirm/Classes/FadeScroll/FadeScrollViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 final class FadeScrollViewController: UIViewController {
+
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var fadeView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+    }
+}
+
+// MARK: UIScrollViewDelegate
+extension FadeScrollViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let scrollHeight = scrollView.contentSize.height
+        let screenHeight = scrollView.bounds.size.height
+        let length = scrollHeight - screenHeight
+        fadeView.alpha = 1 - (offsetY / length)
     }
 }
